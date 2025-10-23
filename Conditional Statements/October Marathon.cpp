6b0412abcf05a3f7d839c088b0511e9f7c9e75f1
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-10-23 21:22:53 +0600

    Update October Marathon.cpp

--- Conditional Statements/October Marathon.cpp
+++ Conditional Statements/October Marathon.cpp
@@ -7,11 +7,11 @@ int main()
 {
     int x;
     cin >> x;
-    if (x <= 2)
+    if (x < 3)
     {
         cout << "GOLD" << endl;
     }
-    else if (x <= 5)
+    else if (x < 6)
     {
         cout << "SILVER" << endl;
     }
@@ -19,3 +19,4 @@ int main()
         cout << "BRONZE" << endl;
     return 0;
 }
+
